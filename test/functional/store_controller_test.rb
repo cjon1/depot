76570273c8deb4b5ac41d2f66b48cdf75264738c
Colin Jones <colin.jones@phe.gov.uk>
2015-07-10 13:38:55 +0100

    Finished Chapter 8, rake test works ok (with currency test disabled)

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    #assert_select '.price', /\£[,\d]+\.\d\d/ # - this test is illegal - it hates the £ symbol as not part of standard US-ASCII table
+    #assert_select '.price', /\$[,\d]+\.\d\d/ # - this test would work if I hadn't changed my currency to Pounds
   end
 
 end
