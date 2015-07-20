0f7395c5fe94d8792fb48f6976b76c7126498284
Colin Jones <colin.jones@phe.gov.uk>
2015-07-20 16:27:50 +0100

    middle of page 114

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -11,4 +16,18 @@ class Product < ActiveRecord::Base
   validates_length_of :title, :minimum => 10, :too_short => "Must have at least 10 characters!" #did this work??
   validates :title, length: {minimum: 10, too_short: ": Your title is too short and you are too stupid. Moron." } 
   # NB - both of the above validations work! (ie they do the same thing slightly diff ways)
+  
+  private
+  
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+  
+  
 end
