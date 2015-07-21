ef5a69f1a5634a453cc992fdab9229b339c074fb
Colin Jones <colin.jones@phe.gov.uk>
2015-07-21 15:58:25 +0100

    Finished chapter 10 apart from the excercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150710125347) do
+ActiveRecord::Schema.define(:version => 20150721112309) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(:version => 20150710125347) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
